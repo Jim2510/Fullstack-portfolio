bab989407c2497ea534023fa39d97c45fc4f61f8
Jim2510 <gmguglielmino@gmail.com>
2024-07-26 21:36:27 +0200

    donation button fixed

--- src/components/OfferBtn.jsx
+++ src/components/OfferBtn.jsx
@@ -6,7 +6,7 @@ export function OfferBtn() {
     <>
       <Link
         to={"https://buy.stripe.com/bIY6oOeGm1iBaUEbII"}
-        className="group fixed top-60 left-[8.5rem] rounded-full w-[80px] h-[80px] z-[100]"
+        className="group fixed top-60 left-[8.5rem] rounded-full w-[80px] h-[80px]"
       >
         <img src={coffee} alt="" />
         <div className=" justify-center items-center absolute group-hover:flex hidden text-black font-bold -top-12 -right-7 rounded-2xl bg-main-red px-2 py-1 w-[150px] ">
