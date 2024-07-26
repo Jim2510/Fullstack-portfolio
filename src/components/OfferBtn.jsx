7ce94a09ef003a3d7bc5507949e4745fbe4e0984
Jim2510 <gmguglielmino@gmail.com>
2024-07-26 21:34:44 +0200

    donation button fixed

--- src/components/OfferBtn.jsx
+++ src/components/OfferBtn.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import coffee from "../assets/cup.png";
 
 export function OfferBtn() {
   return (
@@ -7,7 +8,7 @@ export function OfferBtn() {
         to={"https://buy.stripe.com/bIY6oOeGm1iBaUEbII"}
         className="group fixed top-60 left-[8.5rem] rounded-full w-[80px] h-[80px] z-[500]"
       >
-        <img src="/src/assets/cup.png" alt="" />
+        <img src={coffee} alt="" />
         <div className=" justify-center items-center absolute group-hover:flex hidden text-black font-bold -top-12 -right-7 rounded-2xl bg-main-red px-2 py-1 w-[150px] ">
           Buy me a coffee!
         </div>
