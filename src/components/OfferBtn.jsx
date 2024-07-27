db0887765f07de939527a8d6ebd41a40d9496195
Jim2510 <gmguglielmino@gmail.com>
2024-07-27 06:30:40 +0200

    bug fixing

--- src/components/OfferBtn.jsx
+++ src/components/OfferBtn.jsx
@@ -5,7 +5,7 @@ export function OfferBtn() {
     <>
       <a
         to={"https://buy.stripe.com/bIY6oOeGm1iBaUEbII"}
-        className="group fixed top-[530px] md:top-64 left-[0.4rem] md:left-[9rem] rounded-full w-[25px] h-[25px] md:w-[60px] md:h-[60px]"
+        className="group fixed top-[520px] md:top-64 left-[0.4rem] md:left-[9rem] rounded-full w-[25px] h-[25px] md:w-[60px] md:h-[60px]"
       >
         <img src={coffee} alt="" />
         <div className=" justify-center items-center absolute group-hover:flex hidden text-black font-bold -top-12 -right-10 rounded-2xl bg-main-red px-2 py-1 w-[150px] ">
