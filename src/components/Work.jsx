c079e0f47cb445785d749235239f8d9df820fc29
Jim2510 <gmguglielmino@gmail.com>
2024-07-27 13:29:58 +0200

    new pj added blog

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import gestify from "../assets/gestify.png";
+import blog from "../assets/Immagine 2024-07-27 131435.png";
 
 export function Work() {
   return (
@@ -53,6 +54,48 @@ export function Work() {
                   <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
                 </svg>
               </a>
+            </div>
+          </div>
+          <div className="w-full grid grid-cols-1 sm:grid-cols-6 gap-0 sm:gap-20 flex-row-reverse mt-20">
+            <div className="col-span-1 sm:col-span-2 h-fit flex justify-center flex-col items-center sm:items-start z-10 py-4 text-end gap-4">
+              <h4 className="font-tech text-second-red text-xl sm:text-base">
+                Fullstack Project
+              </h4>
+              <h2 className=" sm:text-xl font-extrabold text-wrap text-center sm:text-nowrap text-2xl">
+                The Loud Whisper - Forum
+              </h2>
+              <div className="overflow-visible w-full sm:w-[500px] text-justify sm:text-start bg-main-red p-5 relative rounded-md shadow-2xl">
+                <p className="text-sm font-semibold">
+                  &quotFullstack Blog Project&quot è una piattaforma di blogging
+                  creata per offrire soluzioni complete di sviluppo web.
+                  Sviluppata come progetto finale del corso di programmazione
+                  fullstack, questa applicazione integra tecnologie frontend
+                  avanzate con robusti backend e pratiche DevOps per una
+                  gestione ottimale dei contenuti.
+                </p>
+              </div>
+              <h4 className="text-[12px] overflow-visible font-tech text-wrap sm:text-nowrap text-center">
+                Next - Node - Jwt - MongoDB - mongoose
+              </h4>
+              <a href="https://github.com/Jim2510/gestify_business_managment/tree/main">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fillRule="currentColor"
+                  className="bi bi-github fill-white"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                </svg>
+              </a>
+            </div>
+            <div className="col-span-1 sm:col-span-4 h-fit sm:h-[350px] mx-auto  overflow-hidden relative sm:rounded-none rounded-md">
+              <img
+                src={blog}
+                alt="gestify"
+                className="w-full h-full  object-cover sm:object-contain"
+              />
             </div>
           </div>
         </div>
