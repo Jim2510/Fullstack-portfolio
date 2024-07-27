af89e8d525e0556e07c137f049d4761a497225f5
Jim2510 <gmguglielmino@gmail.com>
2024-07-27 06:16:45 +0200

    donation button fixed

--- src/components/OfferBtn.jsx
+++ src/components/OfferBtn.jsx
@@ -1,18 +1,17 @@
-import { Link } from "react-router-dom";
 import coffee from "../assets/cup.png";
 
 export function OfferBtn() {
   return (
     <>
-      <Link
+      <a
         to={"https://buy.stripe.com/bIY6oOeGm1iBaUEbII"}
-        className="group fixed top-[500px] md:top-60 left-[0.4rem] md:left-[8.5rem] rounded-full w-[25px] h-[25px] md:w-[80px] md:h-[80px]"
+        className="group fixed top-[530px] md:top-64 left-[0.4rem] md:left-[9rem] rounded-full w-[25px] h-[25px] md:w-[60px] md:h-[60px]"
       >
         <img src={coffee} alt="" />
-        <div className=" justify-center items-center absolute group-hover:flex hidden text-black font-bold -top-12 -right-7 rounded-2xl bg-main-red px-2 py-1 w-[150px] ">
+        <div className=" justify-center items-center absolute group-hover:flex hidden text-black font-bold -top-12 -right-10 rounded-2xl bg-main-red px-2 py-1 w-[150px] ">
           Buy me a coffee!
         </div>
-      </Link>
+      </a>
     </>
   );
 }
