fc4e3e9ee19f44a3867ff05f1577baebb508ebb1
Jim2510 <gmguglielmino@gmail.com>
2024-11-23 14:44:34 +0100

    merging changes

---
+++ src/components/OtherInfo.jsx
@@ -0,0 +1,11 @@
+export function OtherInfo() {
+    return(
+        <>
+        <div className="w-full min-h-screen bg-black">
+            <div className="flex items-center justify-between w-full">
+
+            </div>
+        </div>
+        </>
+    )
+}
